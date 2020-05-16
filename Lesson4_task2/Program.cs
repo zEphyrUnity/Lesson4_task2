37452cb4ede7a3a33be643277b68d28d16e9816b
Igor <61108251+zEphyrUnity@users.noreply.github.com>
2020-05-16 17:12:01 +0300

    Добавьте файлы проекта.

---
+++ Lesson4_task2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+//Дана коллекция List<T>. Требуется подсчитать, сколько раз каждый элемент встречается в данной коллекции: для целых чисел;
+
+namespace Lesson4_task2
+{
+    class Program
+    {
+        const int count = 10;
+        static int counter;
+
+        static Random rnd = new Random();
+        static List<int> someList = new List<int>(count);
+        static Dictionary<int, int> someDictionary = new Dictionary<int, int>();
+
+        private static int Randomizer(Random rnd)
+        {
+            return rnd.Next(1, 10);
+        }
+
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < someList.Capacity; i++)
+                someList.Add(Randomizer(rnd));
+
+            for(int i = 0; i < someList.Count; i++)
+            {
+                counter = 1;
+                for (int j = i + 1; j < someList.Count -1; j++)
+                {
+                    if (someList[i] == someList[j])
+                        counter++;
+                }
+
+                if (!someDictionary.ContainsKey(someList[i]))
+                    someDictionary.Add(someList[i], counter);
+            }
+
+            foreach(var item in someList)
+                Console.WriteLine(item);
+
+            foreach (KeyValuePair<int, int> kvp in someDictionary)
+                Console.WriteLine($"{kvp.Key}: входит {kvp.Value} раз");
+        }
+    }
+}
